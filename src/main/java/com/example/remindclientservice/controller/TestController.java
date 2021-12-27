c036514f9015cabdb7abb58774f7b2d7ae8d853c
GyuHyeong <cjsworbehd13@gmail.com>
2021-12-28 00:59:41 +0900

    Local 환경 카프카 연동

--- src/main/java/com/example/remindclientservice/controller/TestController.java
+++ src/main/java/com/example/remindclientservice/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.example.remindclientservice.controller;
 
+import com.example.remindclientservice.kafka_publisher.KafkaPublisher;
+import com.example.remindclientservice.payload.KafkaTestRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.openfeign.FeignClient;
@@ -7,6 +9,8 @@ import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
@@ -16,8 +20,8 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 public class TestController {
-
     private final Environment env;
+    private final KafkaPublisher kafkaPublisher;
     @GetMapping("/user")
     public ResponseEntity<String> getUser(){
         return new ResponseEntity<>("'username': 'gyus', 'mobile':'10-1234-1234'", HttpStatus.OK);
@@ -32,5 +36,12 @@ public class TestController {
     @GetMapping("/properties/all")
     public ResponseEntity<List<String>> getProperties(){
         return new ResponseEntity<>(Arrays.asList(env.getProperty("test.token-expire"), env.getProperty("test.salt")), HttpStatus.OK);
+    }
+
+    @PostMapping("/publish/test")
+    public ResponseEntity<Boolean> kafkaPublishTest(@RequestBody KafkaTestRequest kafkaTestRequest){
+        log.info("카프카 퍼블리쉬 테스트 메소드 진입");
+        kafkaPublisher.publishTest(kafkaTestRequest.getMessage());
+        return new ResponseEntity<>(Boolean.TRUE, HttpStatus.OK);
     }
 }
